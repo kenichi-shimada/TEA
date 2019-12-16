287e9d191fe7c5366df3d66f3f98d35cb128a0e3
kenichi-shimada <kenichi_shimada@hms.harvard.edu>
2019-12-16 17:27:35 -0500

    prep files added

---
+++ src/00_prep/init.r
@@ -0,0 +1,5 @@
+root_dir <-  "/Users/ks389/Dropbox (HMS)/projects/02 TEA/TEA"
+data_dir <- "/Users/ks389/Dropbox (HMS)/projects/02 TEA/TEA/data"
+rda_dir <- "/Users/ks389/Dropbox (HMS)/projects/02 TEA/TEA/rdas"
+src_dir <- "/Users/ks389/Dropbox (HMS)/projects/02 TEA/TEA/src"
+
---
+++ src/00_prep/install_victor.sh
@@ -0,0 +1,2 @@
+cd "/Users/ks389/Dropbox (HMS)/projects/02 TEA/TEA/src"
+R CMD INSTALL VICTOR
