f5add618d185f60e4394e28810c3932b0630391b
kenichi-shimada <kenichi_shimada@hms.harvard.edu>
2019-12-17 12:00:04 -0500

    modified plate.image to make color flexible

--- src/VICTOR/R/plate.image.R
+++ src/VICTOR/R/plate.image.R
@@ -1,9 +1,6 @@
-plate.image <-
-function(plate,main="Plate Heatmap",...){
-  op <- par()
-  par(mar=c(3,3,2,1))
+plate.image <- function(plate,main="Plate Heatmap",col=heat.colors(256),...){
   par(tcl=0)
-  image(1:24,1:16,t(plate[16:1,]),col=heat.colors(256),axes=F,xlab="",ylab="",
+  image(1:24,1:16,t(plate[16:1,]),col=col,axes=F,xlab="",ylab="",
         main=main,asp=1)
   sapply(2:16,function(x)lines(c(0.5,24.5),rep(x-.5,2),lty=1,col="grey50"))
   sapply(2:24,function(x)lines(rep(x-.5,2),c(0.5,16.5),lty=1,col="grey50"))
@@ -13,6 +10,6 @@ function(plate,main="Plate Heatmap",...){
   text(-0.5,16:1,LETTERS[seq(16)],cex.axis=.6,las=1)
   text(1:24,-0.5,seq(24),cex.axis=.5,las=1)
   par(xpd=F)
-  ##par(op)
+  invisible()
 }
 
