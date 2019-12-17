293caf7924c504053e9394203a228109043d74af
kenichi-shimada <kenichi_shimada@hms.harvard.edu>
2019-12-17 11:59:39 -0500

    modified prep

--- src/00_prep/init.r
+++ src/00_prep/init.r
@@ -1,5 +1,6 @@
 root_dir <-  "/Users/ks389/Dropbox (HMS)/projects/02 TEA/TEA"
 data_dir <- "/Users/ks389/Dropbox (HMS)/projects/02 TEA/TEA/data"
+plot_dir <- "/Users/ks389/Dropbox (HMS)/projects/02 TEA/TEA/plots"
 rda_dir <- "/Users/ks389/Dropbox (HMS)/projects/02 TEA/TEA/rdas"
 src_dir <- "/Users/ks389/Dropbox (HMS)/projects/02 TEA/TEA/src"
 
