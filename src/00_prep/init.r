4102fa6ea3817a25b526b2b76e6574f7a7649de7
kenichi-shimada <kenichi_shimada@hms.harvard.edu>
2019-12-27 17:47:54 -0500

    removed init.r

--- src/00_prep/init.r
+++
@@ -1,6 +0,0 @@
-root_dir <-  "/Users/ks389/Dropbox (HMS)/projects/02 TEA/TEA"
-data_dir <- "/Users/ks389/Dropbox (HMS)/projects/02 TEA/TEA/data"
-plot_dir <- "/Users/ks389/Dropbox (HMS)/projects/02 TEA/TEA/plots"
-rda_dir <- "/Users/ks389/Dropbox (HMS)/projects/02 TEA/TEA/rdas"
-src_dir <- "/Users/ks389/Dropbox (HMS)/projects/02 TEA/TEA/src"
-
